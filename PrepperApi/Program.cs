5f34f90b180e7b5414c3a949f497f1eb5032aaf9
Thomas Larsen <larsen.thomas.dk@gmail.com>
2025-12-21 01:38:53 +0100

    Added CORS to any policy

--- PrepperApi/Program.cs
+++ PrepperApi/Program.cs
@@ -8,6 +8,18 @@ builder.Services.AddControllers();
 //builder.Services.AddOpenApi();
 builder.Services.AddSingleton<IRepository<Ingredient>, IngrediantRepo>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+// Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -25,9 +37,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+    
